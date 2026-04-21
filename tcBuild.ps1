72e5d970d072189036c27815f438dfc7e1c5fcde
Rienk de Jong <rienk@rienkdejong.nl>
2026-04-21 13:55:47 +0200

    Create tcBuild.ps1

---
+++ tcBuild.ps1
@@ -0,0 +1,14 @@
+$dte = new-object -com "TcXaeShell.DTE.17.0" # XaeShell64 COM ProgId
+$dte.SuppressUI = $true # suppress VS interface
+
+# open solution file
+echo "Opening solution"
+$slnPath = "$pwd\TwinCAT Project\TwinCAT Project.sln"
+$sln = $dte.Solution
+$sln.Open($slnPath)
+
+echo "Building TwinCAT project"
+$sln.SolutionBuild.Build($true)
+
+echo "Exiting..."
+$dte.Quit()
